01deb6b8518cae2a0a408d9f314c70e46d2ed7f9
John McConnell <johnmc0831@gmail.com>
2016-01-31 14:02:00 -0500

    Initial Commit.

---
+++ FTL/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace FTL
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/fonts/font-awesome/css/font-awesome.css",
+                      "~/assets/fancybox/jquery.fancybox.css?v=2.1.5",
+                      "~/Content/css/global-style.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
