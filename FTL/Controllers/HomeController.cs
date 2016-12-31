9076f2160967bf0f9776d4763cbd6df3593aa0ce
John McConnell <johnmc0831@gmail.com>
2016-12-31 14:00:08 -0500

    Activated Feedback form on Support view.  Used SendGrid mail service.

--- FTL/Controllers/HomeController.cs
+++ FTL/Controllers/HomeController.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
 
 namespace FTL.Controllers
 {
+    using System.Net.Mail;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -24,8 +29,24 @@ namespace FTL.Controllers
         }
 
         [HttpPost]
-        public ActionResult Support(FormCollection fc)
+        public async Task<ActionResult> Support(FormCollection fc)
         {
+            //send email to info@patientsafetymovement.org
+            //SendGridAPI KEY:  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
+            string apiKey = "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";
+            dynamic sg = new SendGridAPIClient(apiKey);
+            Email from = new Email("patientaider@ftltech.org", "PatientAider Feedback Engine");
+            string subject = fc["Subject"];
+            Email to = new Email("info@patientsafetymovement.org");
+            string message = "The following PatientAider feedback message has been received.  "
+                             + $"This feedback was sent by {fc["Name"]} with email address {fc["EMail"]} "
+                             + $"at {DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()}:"
+                             + Environment.NewLine + Environment.NewLine
+                             + $"{fc["Subject"]}" + Environment.NewLine + $"{fc["YourMessage"]}";
+            Content content = new Content("text/plain", message);
+            Mail mail = new Mail(from, subject, to, content);
+
+            dynamic response = await sg.client.mail.send.post(requestBody: mail.Get());
             return View("Index");
         }
     }
